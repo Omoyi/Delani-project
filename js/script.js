10080afab66ab2510f31599a6757a969932b641d
Omoyi <urakhel7@gmail.com>
2019-07-26 21:50:47 +0200

    add function for jquery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 $(document).ready(function(){
   $("#desi").click(function(){
-    $("b").toggle
-  })
+    $("#desi").hide();
+    $(".aa").show();
+  });
+  $(".aa").click(function(){
+    $(".aa").hide();
+    $("#desi").show();
+  });
+  $("#dev").click(function(){
+    $("#dev").hide();
+    $(".b").show();
+  });
+  $(".b").click(function(){
+    $(".b").hide();
+    $("#dev").show();
+  });
+  $("#prod").click(function(){
+    $("#prod").hide();
+    $(".c").show();
+  });
+  $(".c").click(function(){
+    $(".c").hide();
+    $("#prod").show();
+  });
+});
 
-})
+
+
+
