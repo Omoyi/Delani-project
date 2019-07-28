07669f0eb3cf38a0b8c7db7831bd9b0e29c016b0
Omoyi <urakhel7@gmail.com>
2019-07-28 20:06:19 +0200

    add variables and other things

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Back-end logic / Business logic 
+// Back-end logic / Business logic
 $(document).ready(function(){
   $("#desi").click(function(){
     $("#desi").hide();
@@ -24,8 +24,21 @@ $(document).ready(function(){
     $(".c").hide();
     $("#prod").show();
   });
+  $(n11).mouseOver(function(){
+    $("n1").show();
+    $("n1").addclass("nb1");
+  });
 });
 
+$(document).ready(function(event){
+  $("#container").submit(function(){
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var comment = $("textarea#comment").val();
+    alert(name + "we have received your message. Thank you for reaching out to us.");
+    event.preventDefault();
+  });
+});
 
 
 
